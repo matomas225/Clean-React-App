93f749531b41b1b15d2c68b3e1fd67ca38b06cde
Matomas <Matomas225@gmail.com>
2021-10-11 21:11:39 +0300

    gp pages fixed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
+import Header from './Components/Header';
 import Nav from './Components/Nav';
 
 function App() {
-  return ( <>
-  <Nav/>
-    <header>
-      <h1>Your website is an investment in<br/> the success of your business.</h1>
-      <p>We are here to help you succeed.</p>
-      <button>LET'S WORK TOGETHER</button>
-    </header>
-  </>
+  return ( <div style={{overflow: "hidden"}}>
+    <Nav/>
+  <Header/>
+  </div>
   );
 }
 
---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+function Header()
+    {
+    return <header>
+      <h1>Your website is an investment in<br/> the success of your business.</h1>
+      <p>We are here to help you succeed.</p>
+      <button>LET'S WORK TOGETHER</button>
+      <div className="Triangle"></div>
+    </header>
+}
+export default Header;
