e7aea39426e6c7185d8cafd995a5843f0de8105a
Matomas <Matomas225@gmail.com>
2021-10-11 20:22:46 +0300

    nav and header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import Nav from './Components/Nav';
 
 function App() {
-  return (
-    <div></div>
+  return ( <>
+  <Nav/>
+    <header>
+      <h1>Your website is an investment in<br/> the success of your business.</h1>
+      <p>We are here to help you succeed.</p>
+      <button>LET'S WORK TOGETHER</button>
+    </header>
+  </>
   );
 }
 
---
+++ src/Components/Nav.js
@@ -0,0 +1,13 @@
+import "./Nav.css";
+
+function Nav(){
+    return <nav>
+        <ul>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Case Studies</a></li>
+            <li><a href="">Contact Us</a></li>
+        </ul>
+    </nav>
+}
+
+export default Nav;
