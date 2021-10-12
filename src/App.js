f2faa498dcbaa21e481a1288239332e09b67f3cc
Matomas <Matomas225@gmail.com>
2021-10-12 15:19:53 +0300

    Animations Main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+//SITE COPY: https://www.rekupe.com/
 import './App.css';
 import Header from './Components/Header';
+import Main from './Components/Main';
 import Nav from './Components/Nav';
-
 function App() {
-  return ( <div style={{overflow: "hidden"}}>
+  return ( <div>
     <Nav/>
   <Header/>
+  <Main/>
   </div>
   );
 }
